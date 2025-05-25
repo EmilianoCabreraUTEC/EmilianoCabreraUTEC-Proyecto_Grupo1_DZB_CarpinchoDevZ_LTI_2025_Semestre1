a312bdcd4f9f1fa88c2c440b478b52924db9a6a1
Leti Rojí <leticia.roji@estudiantes.utec.edu.uy>
2025-05-25 01:52:56 -0300

    Comencé con la clase Instancia (necesitamos crear la subclase estudiante)

--- src/CasoEstudioUTEC/InstanciaSeguimiento.java
+++ src/CasoEstudioUTEC/InstanciaSeguimiento.java
@@ -37,6 +37,7 @@ public class InstanciaSeguimiento {
         return UUID.randomUUID().toString(); //no se como se arma el metodo para que sean con orden secuencial
     }
 
+
     public InstanciaSeguimiento clonar() {
         return new InstanciaSeguimiento(
                 this.titulo + " (copia)",
