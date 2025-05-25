efc06e01ce3e940199a40da3fa8edba3e383ae67
lucasacunautec <lucas.acuna@estudiantes.utec.edu.uy>
2025-05-25 14:08:05 -0300

    Clase Tester para probar creacion de instancias ychequear si estan bien definidas las clases

---
+++ src/CasoEstudioUTEC/Testear.java
@@ -0,0 +1,13 @@
+package CasoEstudioUTEC;
+
+public class Testear {
+    public static void main(String[] args) {
+        Recordatorio r = new Recordatorio("Reunión", "Académico", "10-05-2025", "14:30", "Única");
+        System.out.println(r);
+    }
+}
+
+
+/*
+Usar esta clase para testear creacion de instancias de las clases
+si da problema otra clase pueden comentar localmente todo el codigo y correr solamente la instancia */
