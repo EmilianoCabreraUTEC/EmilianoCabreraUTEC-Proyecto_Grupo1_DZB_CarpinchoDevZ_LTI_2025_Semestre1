fc12e989ae5da771129c123592894d4539428a2b
Leti Rojí <leticia.roji@estudiantes.utec.edu.uy>
2025-05-30 17:25:00 -0300

    Clase para administradoes del sistema y agregué en Usuario el método para cambiar contraseña

--- src/CasoEstudioUTEC/Usuario.java
+++ src/CasoEstudioUTEC/Usuario.java
@@ -162,6 +162,15 @@ public abstract class Usuario {
     }
     //setters de los permisos, solo pueden accederse interamente por la clase
 
+    // Metodo para cambiar la contraseña (como aclaramos que respete el formato anterior y que no pueda guardar la misma?)
+    public boolean cambiarContrasenia(String nuevaContrasenia) {
+        if (nuevaContrasenia == null || nuevaContrasenia.length() <12) {
+            System.out.println("La contraseña debe tener al menos 12 caracteres.");
+            return false;
+        }
+        this.contrasenia = nuevaContrasenia;
+        return true;
+    }
 
     //to string:
     @Override
